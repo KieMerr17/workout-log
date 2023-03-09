4ba7ebfa8305e116a7cfec872db613373ec53e25
KieMerr17 <kieran.merrett@hotmail.com>
2023-03-09 12:16:08 +0000

    function: adjust get_users_name() and get_users_age() to validate input is just letters or numbers

--- run.py
+++ run.py
@@ -13,18 +13,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('workout-log')
 
-# Get all sheet information from Google Sheets
-
-
-# exercises = SHEET.worksheet('exercises')
-# exercise_sheet = exercises.get_all_values()
-
-# results = SHEET.worksheet('results')
-# results_sheet = results.get_all_values()
-
-# workouts = SHEET.worksheet('workouts')
-# workouts_sheet = workouts.get_all_values()
-
 # Start of Terminal process
 print("Welcome to your workout log\n")
 
@@ -33,23 +21,31 @@ def get_users_name():
     """
     Function gets the First and Last name of the user.
     """
-    user_input = input("Please enter your First and Last name..\n")
-    return user_input
+    while True:
+        user_input = input("Please enter your First and Last name..\n")
+        if user_input.replace(" ", "").isalpha():
+            # Check that the input contains only letters after removing any spaces
+            return user_input
+        else:
+            print("Err: Please enter only letters.")
 
 
 def get_users_age():
     """
     Function gets the age of the user.
     """
-    print("Please enter your age.")
-    print("For example '31'")
-    user_age = input("")
-    return user_age
+    while True:
+        user_input = input("Please enter your age.\n")
+        if user_input.replace(" ", "").isdigit():
+            # Check that the input contains only numbers after removing any spaces
+            return user_input
+        else:
+            print("Err: Please enter only numbers.")
 
 
-def validate_username(user):
+def validate_user(user):
     """
-    Function checks to see if users inputted name is already on 
+    Function checks to see if users inputted name is already on
     the spreadsheet.
     If NOT then it appends the name to the sheet.
     """
@@ -62,11 +58,11 @@ def validate_username(user):
         all_names.append_row([user, user_age])
 
         print("*New profile created*")
-    
+
 
 def main():
     user_name = get_users_name()
-    validate_username(user_name)
-    
+    validate_user(user_name)
+
 
 main()
