20293021542ef6c82391d22d05babfaee32afbba
KieMerr17 <kieran.merrett@hotmail.com>
2023-03-16 10:59:24 +0000

    file: adjust colors.py file to pass through PEP8 validation

--- colors.py
+++ colors.py
@@ -5,9 +5,8 @@ init(autoreset=True)
 
 
 class Color:
-    # 
+    # Colors to be used in run.py for text highlighting
     YELLOW = "\033[1;33;48m"
     RED = "\033[1;31;48m"
     GREEN = "\033[1;32;48m"
     BLUE = "\033[1;34;48m"
-    
